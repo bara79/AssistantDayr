7549063932d5d3f83a0da269c96a76c920ac4fc2
bara79 <bara_x@mail.ru>
2022-03-15 16:40:32 +0300

    создание экрана квест экрана

--- app/src/main/java/com/bara_x/assistantdayr/MainActivity.kt
+++ app/src/main/java/com/bara_x/assistantdayr/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.bara_x.assistantdayr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonBtnQuest = findViewById<Button>(R.id.btnQuest)
+        val textViewQuest = findViewById<TextView>(R.id.tvQuest)
+        val imageViewQuest = findViewById<ImageView>(R.id.ivQuest)
+
+            buttonBtnQuest.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this@MainActivity, R.anim.scale)
+            buttonBtnQuest.startAnimation(animation)
+            textViewQuest.startAnimation(animation)
+            imageViewQuest.startAnimation(animation)
+            val intent = Intent(this, QuestListActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
+
+
+
+
---
+++ app/src/main/java/com/bara_x/assistantdayr/QuestListActivity.kt
@@ -0,0 +1,17 @@
+package com.bara_x.assistantdayr
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+
+class QuestListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quest_list)
+
+
+
+
+    }
+}
