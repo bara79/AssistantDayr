c51f33212fdd7748236661da93805ade6ead9daf
bara79 <bara_x@mail.ru>
2022-04-01 16:29:40 +0300

    создание экранов

--- app/src/main/java/com/bara_x/assistantdayr/transport/adapterViewPager/PagerAdapter.kt
+++ app/src/main/java/com/bara_x/assistantdayr/transport/adapterViewPager/PagerAdapter.kt
@@ -3,21 +3,21 @@ package com.bara_x.assistantdayr.transport.adapterViewPager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.bara_x.assistantdayr.transport.screen.BicycleFirstFragment
-import com.bara_x.assistantdayr.transport.screen.BicycleTheereFragment
-import com.bara_x.assistantdayr.transport.screen.BicycleTooFragment
+import com.bara_x.assistantdayr.transport.screen.*
 
-class PagerAdapter (fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+class PagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
-       return 3
+        return 5
     }
 
     override fun createFragment(position: Int): Fragment {
-        return  when (position){
+        return when (position) {
 
-            0->BicycleFirstFragment()
-            1->BicycleTooFragment()
-            else->BicycleTheereFragment()
+            0 -> BicycleFirstFragment()
+            1 -> BicycleTooFragment()
+            2 -> BicycleTheereFragment()
+            3 -> MotorizedCartBrokenFragment()
+            else->MotorizedCartFragment()
         }
 
     }
