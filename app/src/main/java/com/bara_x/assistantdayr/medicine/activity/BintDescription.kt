7f6bbf79a96e20cbfdd5432cfd9c6bdb7edbb812
Alexandr <bara_x@mail.ru>
2024-02-07 11:05:29 +0300

    добавил медицину

---
+++ app/src/main/java/com/bara_x/assistantdayr/medicine/activity/BintDescription.kt
@@ -0,0 +1,12 @@
+package com.bara_x.assistantdayr.medicine.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bara_x.assistantdayr.R
+//ни чего не нужно делать.
+class BintDescription : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medicine_description)
+    }
+}
